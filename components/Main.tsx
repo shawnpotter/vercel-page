e6a9a8414b88a75f9948b169c7c26f1bf9af44e3
Shawn Potter <potter.shawn@student.greenriver.edu>
2023-01-21 18:51:08 -0800

    added Main component

---
+++ components/Main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Greeting from './Main/Greeting'
+
+export default function Main() {
+	return (
+		<div className='w-full h-screen text-center'>
+			<Greeting/>
+		</div>
+	)
+}
