8d75818e0708616c77ef56cb7e9fe4ef93771a96
Shawn Potter <potter.shawn@student.greenriver.edu>
2023-01-21 18:07:42 -0800

    created navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,35 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+export default function Navbar() {
+	return(
+		<div className='fixed w-full h-20 shadow-xl z-[100]'>
+			<div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
+				<Image 
+					src="" 
+					alt="" 
+				/>
+				<div>
+				<ul className='hidden md:flex'>
+					<Link href='/'>
+						<li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
+					</Link>
+					<Link href='/'>
+						<li className='ml-10 text-sm uppercase hover:border-b'>About</li>
+					</Link>
+					<Link href='/'>
+						<li className='ml-10 text-sm uppercase hover:border-b'>Skills</li>
+					</Link>
+					<Link href='/'>
+						<li className='ml-10 text-sm uppercase hover:border-b'>Projects</li>
+					</Link>
+					<Link href='/'>
+						<li className='ml-10 text-sm uppercase hover:border-b'>Contact</li>
+					</Link>
+				</ul>
+			</div>
+			</div>
+		</div>
+	)
+}
