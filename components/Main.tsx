962e9de0dc94b72b6c587664a12dcf87eebc2be4
Shawn Potter <potter.shawn@student.greenriver.edu>
2023-01-21 23:33:39 -0800

    updated to preferred bracket style

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Greeting from './Main/Greeting'
 
-export default function Main() {
+export default function Main() 
+{
 	return (
 		<div className='w-full h-screen text-center'>
 			<Greeting/>
