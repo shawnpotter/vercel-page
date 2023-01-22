b95a3f23cae7b9c222661762c688f22327490abe
Shawn Potter <potter.shawn@student.greenriver.edu>
2023-01-21 18:51:19 -0800

    Added Greeting component

---
+++ components/Main/Greeting.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Greeting() {
+	return (
+		<div className='max-w-7xl w-full h-full mx-auto p-2 flex justify-center items-center'>
+			<div>
+				<p className='uppercase text-sm tracking-widest text-gray-50'></p>
+			</div>
+		</div>
+	)
+}
