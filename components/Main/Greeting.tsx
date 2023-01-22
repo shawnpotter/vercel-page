b4669b0bb7455ee85742aeae61b07f60b1d44730
Shawn Potter <potter.shawn@student.greenriver.edu>
2023-01-21 23:32:45 -0800

    added anchor tags to social buttons

--- components/Main/Greeting.tsx
+++ components/Main/Greeting.tsx
@@ -3,7 +3,8 @@ import { AiOutlineMail } from 'react-icons/ai';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
-export default function Greeting() {
+export default function Greeting() 
+{
 	return (
 		<div className='max-w-7xl w-full h-full mx-auto p-2 flex justify-center items-center'>
 			<div>
@@ -22,16 +23,25 @@ export default function Greeting() {
 				</p>
 				<div className='flex items-center justify-between max-w-xs m-auto py-4'>
 					<div className='rounded-full shadow shadow-black p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-						<FaLinkedin />
+						<a href='https://www.linkedin.com/in/shawn-potter/' aria-label="Link to Shawn's LinkedIn page">
+							<FaLinkedin />
+						</a>
 					</div>
 					<div className='rounded-full shadow shadow-black p-6 cursor-pointer hover:scale-105 ease-in duration-300'>
-						<FaGithub />
+						<a href='https://github.com/shawnpotter' aria-label="Link to Shawn's GitHub page">
+							<FaGithub />
+						</a>
 					</div>
 					<div className='rounded-full shadow shadow-black p-6 cursor-pointer hover:scale-105 ease-in duration-300'>
-						<AiOutlineMail />
+						<a href='mailto:shawnpotter.inquires@gmail.com' aria-label="Click to send an email to Shawn">
+							<AiOutlineMail />
+						</a>
 					</div>
 					<div className='rounded-full shadow shadow-black p-6 cursor-pointer hover:scale-105 ease-in duration-300'>
-						<BsFillPersonLinesFill />
+						{/* This will eventually be a link */}
+						{/* <a href='/' aria-label=""> */}
+							<BsFillPersonLinesFill />
+						{/* </a> */}
 					</div>
 				</div>
 			</div>
