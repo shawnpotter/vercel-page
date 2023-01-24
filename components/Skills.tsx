674451d97af4f93a4947a33805324ca658054ea2
Shawn Potter <potter.shawn@student.greenriver.edu>
2023-01-24 15:19:42 -0800

    created Database category. added mongodb, hibernate

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -4,7 +4,7 @@ import {
 } from 'react-icons/io5'
 import { FaJava } from 'react-icons/fa'
 import { 
-	SiBootstrap, SiCsharp, SiDocker, SiGithub, SiGraphql, SiJetbrains, SiJquery, SiMysql, SiNextdotjs, SiPhp, SiPostman, SiSpring, SiTailwindcss, SiTypescript, SiVisualstudio, SiVisualstudiocode 
+	SiBootstrap, SiCsharp, SiDocker, SiGithub, SiGraphql, SiHibernate, SiJetbrains, SiJquery, SiMongodb, SiMysql, SiNextdotjs, SiPhp, SiPostman, SiSpring, SiTailwindcss, SiTypescript, SiVisualstudio, SiVisualstudiocode 
 } from 'react-icons/si'
 
 export default function Skills() 
@@ -23,10 +23,10 @@ export default function Skills()
 				<div className='grid sm:grid-cols-2 lg:grid-cols-4 gap-8'>
 				
 					{/* Candidate for Componentization */}
-					<div className='p-6 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300 sm:p-2'>
+					<div className='p-2 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300'>
 						<div className='grid grid-cols-2 gap-4 justify-center items-center'>
 							<div className='m-auto'>
-								<IoLogoHtml5 size={60} color='#E44D26' className='w-'/>
+								<IoLogoHtml5 size={50} color='#E44D26' className='w-'/>
 							</div>
 							<div>
 								<p className='text-xl'>HTML</p>
@@ -34,10 +34,10 @@ export default function Skills()
 						</div>
 					</div>
 
-					<div className='p-6 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300 sm:p-2'>
+					<div className='p-2 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300'>
 						<div className='grid grid-cols-2 gap-4 justify-center items-center'>
 							<div className='m-auto'>
-								<IoLogoCss3 size={60} color='#264DE4'/>
+								<IoLogoCss3 size={50} color='#264DE4'/>
 							</div>
 							<div>
 								<p className='text-xl'>CSS</p>
@@ -45,10 +45,10 @@ export default function Skills()
 						</div>
 					</div>
 
-					<div className='p-6 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300 sm:p-2'>
+					<div className='p-2 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300'>
 						<div className='grid grid-cols-2 gap-4 justify-center items-center'>
 							<div className='m-auto'>
-								<IoLogoJavascript size={60} color='Gold'/>
+								<IoLogoJavascript size={50} color='Gold'/>
 							</div>
 							<div>
 								<p className='text-xl'>JavaScript</p>
@@ -56,10 +56,10 @@ export default function Skills()
 						</div>
 					</div>
 
-					<div className='p-6 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300 sm:p-2'>
+					<div className='p-2 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300'>
 						<div className='grid grid-cols-2 gap-4 justify-center items-center'>
 							<div className='m-auto'>
-								<FaJava size={60} color='#5382A1'/>
+								<FaJava size={50} color='#5382A1'/>
 							</div>
 							<div>
 								<p className='text-xl'>Java</p>
@@ -67,21 +67,10 @@ export default function Skills()
 						</div>
 					</div>
 
-					<div className='p-6 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300 sm:p-2'>
+					<div className='p-2 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300'>
 						<div className='grid grid-cols-2 gap-4 justify-center items-center'>
 							<div className='m-auto'>
-								<SiMysql size={60} color='#00618A'/>
-							</div>
-							<div>
-								<p className='text-xl'>MySQL</p>
-							</div>
-						</div>
-					</div>
-
-					<div className='p-6 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300 sm:p-2'>
-						<div className='grid grid-cols-2 gap-4 justify-center items-center'>
-							<div className='m-auto'>
-								<SiTypescript size={60} color='#3178C6'/>
+								<SiTypescript size={50} color='#3178C6'/>
 							</div>
 							<div>
 								<p className='text-xl'>TypeScript</p>
@@ -89,10 +78,10 @@ export default function Skills()
 						</div>
 					</div>
 
-					<div className='p-6 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300 sm:p-2'>
+					<div className='p-2 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300'>
 						<div className='grid grid-cols-2 gap-4 justify-center items-center'>
 							<div className='m-auto'>
-								<SiPhp size={60} color='#4f5b93'/>
+								<SiPhp size={50} color='#4f5b93'/>
 							</div>
 							<div>
 								<p className='text-xl'>PHP</p>
@@ -100,10 +89,10 @@ export default function Skills()
 						</div>
 					</div>
 
-					<div className='p-6 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300 sm:p-2'>
+					<div className='p-2 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300'>
 						<div className='grid grid-cols-2 gap-4 justify-center items-center'>
 							<div className='m-auto'>
-								<SiCsharp size={60} color='#189F20'/>
+								<SiCsharp size={50} color='#189F20'/>
 							</div>
 							<div>
 								<p className='text-xl'>C#</p>
@@ -118,10 +107,10 @@ export default function Skills()
 				</h3>
 				<div className='grid sm:grid-cols-2 lg:grid-cols-4 gap-8'>
 
-					<div className='p-6 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300 sm:p-2'>
+					<div className='p-2 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300'>
 						<div className='grid grid-cols-2 gap-4 justify-center items-center'>
 							<div className='m-auto'>
-								<IoLogoReact size={60} color='SkyBlue'/>
+								<IoLogoReact size={50} color='SkyBlue'/>
 							</div>
 							<div>
 								<p className='text-xl'>React</p>
@@ -129,10 +118,10 @@ export default function Skills()
 						</div>
 					</div>
 
-					<div className='p-6 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300 sm:p-2'>
+					<div className='p-2 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300'>
 						<div className='grid grid-cols-2 gap-4 justify-center items-center'>
 							<div className='m-auto'>
-								<SiNextdotjs size={60} color='White'/>
+								<SiNextdotjs size={50} color='White'/>
 							</div>
 							<div>
 								<p className='text-xl'>Next.js</p>
@@ -140,10 +129,10 @@ export default function Skills()
 						</div>
 					</div>
 
-					<div className='p-6 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300 sm:p-2'>
+					<div className='p-2 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300'>
 						<div className='grid grid-cols-2 gap-4 justify-center items-center'>
 							<div className='m-auto'>
-								<IoLogoNodejs size={60} color='#84CE24'/>
+								<IoLogoNodejs size={50} color='#84CE24'/>
 							</div>
 							<div>
 								<p className='text-xl'>Node.js</p>
@@ -151,10 +140,10 @@ export default function Skills()
 						</div>
 					</div>
 
-					<div className='p-6 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300 sm:p-2'>
+					<div className='p-2 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300'>
 						<div className='grid grid-cols-2 gap-4 justify-center items-center'>
 							<div className='m-auto'>
-								<SiJquery size={60} color='#0868AB'/>
+								<SiJquery size={50} color='#0868AB'/>
 							</div>
 							<div>
 								<p className='text-xl'>jQuery</p>
@@ -162,10 +151,10 @@ export default function Skills()
 						</div>
 					</div>
 
-					<div className='p-6 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300 sm:p-2'>
+					<div className='p-2 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300'>
 						<div className='grid grid-cols-2 gap-4 justify-center items-center'>
 							<div className='m-auto'>
-								<IoLogoSass size={60} color='#CD6799'/>
+								<IoLogoSass size={50} color='#CD6799'/>
 							</div>
 							<div>
 								<p className='text-xl'>SASS</p>
@@ -173,10 +162,10 @@ export default function Skills()
 						</div>
 					</div>
 
-					<div className='p-6 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300 sm:p-2'>
+					<div className='p-2 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300'>
 						<div className='grid grid-cols-2 gap-4 justify-center items-center'>
 							<div className='m-auto'>
-								<SiSpring size={60} color='#6eb442'/>
+								<SiSpring size={50} color='#6eb442'/>
 							</div>
 							<div>
 								<p className='text-xl'>Spring</p>
@@ -184,10 +173,10 @@ export default function Skills()
 						</div>
 					</div>
 
-					<div className='p-6 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300 sm:p-2'>
+					<div className='p-2 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300'>
 						<div className='grid grid-cols-2 gap-4 justify-center items-center'>
 							<div className='m-auto'>
-								<IoLogoAngular size={60} color='#DD0031'/>
+								<IoLogoAngular size={50} color='#DD0031'/>
 							</div>
 							<div>
 								<p className='text-xl'>Angular</p>
@@ -195,10 +184,10 @@ export default function Skills()
 						</div>
 					</div>
 
-					<div className='p-6 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300 sm:p-2'>
+					<div className='p-2 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300'>
 						<div className='grid grid-cols-2 gap-4 justify-center items-center'>
 							<div className='m-auto'>
-								<SiTailwindcss size={60} color='#38BDF8'/>
+								<SiTailwindcss size={50} color='#38BDF8'/>
 							</div>
 							<div>
 								<p className='text-xl'>TailwindCSS</p>
@@ -206,10 +195,10 @@ export default function Skills()
 						</div>
 					</div>
 
-					<div className='p-6 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300 sm:p-2'>
+					<div className='p-2 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300'>
 						<div className='grid grid-cols-2 gap-4 justify-center items-center'>
 							<div className='m-auto'>
-								<SiBootstrap size={60} color='#8012F1'/>
+								<SiBootstrap size={50} color='#8012F1'/>
 							</div>
 							<div>
 								<p className='text-xl'>Bootstrap</p>
@@ -217,10 +206,10 @@ export default function Skills()
 						</div>
 					</div>
 
-					<div className='p-6 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300 sm:p-2'>
+					<div className='p-2 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300'>
 						<div className='grid grid-cols-2 gap-4 justify-center items-center'>
 							<div className='m-auto'>
-								<SiGraphql size={60} color='DeepPink'/>
+								<SiGraphql size={50} color='DeepPink'/>
 							</div>
 							<div>
 								<p className='text-xl'>GraphQL</p>
@@ -232,13 +221,53 @@ export default function Skills()
 				</div>
 
 				<h3 className='py-4 text-lg'>
+					Databases
+				</h3>
+				<div className='grid sm:grid-cols-2 lg:grid-cols-4 gap-8'>
+
+					<div className='p-2 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300'>
+						<div className='grid grid-cols-2 gap-4 justify-center items-center'>
+							<div className='m-auto'>
+								<SiMysql size={50} color='#00618A'/>
+							</div>
+							<div>
+								<p className='text-xl'>MySQL</p>
+							</div>
+						</div>
+					</div>
+
+					<div className='p-2 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300'>
+						<div className='grid grid-cols-2 gap-4 justify-center items-center'>
+							<div className='m-auto'>
+								<SiMongodb size={50} color='#53B441'/>
+							</div>
+							<div>
+								<p className='text-xl'>MongoDB</p>
+							</div>
+						</div>
+					</div>
+
+					<div className='p-2 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300'>
+						<div className='grid grid-cols-2 gap-4 justify-center items-center'>
+							<div className='m-auto'>
+								<SiHibernate size={50} color='#BCAE79'/>
+							</div>
+							<div>
+								<p className='text-xl'>Hibernate</p>
+							</div>
+						</div>
+					</div>
+
+				</div>
+
+				<h3 className='py-4 text-lg'>
 					Technologies
 				</h3>
 				<div className='grid sm:grid-cols-2 lg:grid-cols-4 gap-8'>
-					<div className='p-6 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300 sm:p-2'>
+					<div className='p-2 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300'>
 						<div className='grid grid-cols-2 gap-4 justify-center items-center'>
 							<div className='m-auto'>
-								<SiVisualstudio size={60} color='#875FC6'/>
+								<SiVisualstudio size={50} color='#875FC6'/>
 							</div>
 							<div>
 								<p className='text-xl'>Visual Studio</p>
@@ -246,10 +275,10 @@ export default function Skills()
 						</div>
 					</div>
 
-					<div className='p-6 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300 sm:p-2'>
+					<div className='p-2 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300'>
 						<div className='grid grid-cols-2 gap-4 justify-center items-center'>
 							<div className='m-auto'>
-								<SiVisualstudiocode size={60} color='#25AEF3'/>
+								<SiVisualstudiocode size={50} color='#25AEF3'/>
 							</div>
 							<div>
 								<p className='text-xl'>VS Code</p>
@@ -257,10 +286,10 @@ export default function Skills()
 						</div>
 					</div>
 
-					<div className='p-6 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300 sm:p-2'>
+					<div className='p-2 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300'>
 						<div className='grid grid-cols-2 gap-4 justify-center items-center'>
 							<div className='m-auto'>
-								<SiJetbrains size={60} color='DeepPink'/>
+								<SiJetbrains size={50} color='DeepPink'/>
 							</div>
 							<div>
 								<p className='text-xl'>JetBrains</p>
@@ -268,10 +297,10 @@ export default function Skills()
 						</div>
 					</div>
 
-					<div className='p-6 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300 sm:p-2'>
+					<div className='p-2 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300'>
 						<div className='grid grid-cols-2 gap-4 justify-center items-center'>
 							<div className='m-auto'>
-								<SiGithub size={60} color='White'/>
+								<SiGithub size={50} color='White'/>
 							</div>
 							<div>
 								<p className='text-xl'>GitHub</p>
@@ -279,10 +308,10 @@ export default function Skills()
 						</div>
 					</div>
 
-					<div className='p-6 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300 sm:p-2'>
+					<div className='p-2 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300'>
 						<div className='grid grid-cols-2 gap-4 justify-center items-center'>
 							<div className='m-auto'>
-								<SiPostman size={60} color='#FD6C35'/>
+								<SiPostman size={50} color='#FD6C35'/>
 							</div>
 							<div>
 								<p className='text-xl'>Postman</p>
@@ -290,10 +319,10 @@ export default function Skills()
 						</div>
 					</div>
 
-					<div className='p-6 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300 sm:p-2'>
+					<div className='p-2 shadow shadow-black rounded-xl hover:scale-105 ease-in duration-300'>
 						<div className='grid grid-cols-2 gap-4 justify-center items-center'>
 							<div className='m-auto'>
-								<SiDocker size={60} color='DodgerBlue'/>
+								<SiDocker size={50} color='DodgerBlue'/>
 							</div>
 							<div>
 								<p className='text-xl'>Docker</p>
