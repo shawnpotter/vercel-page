50f706c83a8104c8421a9083a773f8585b28b45e
Shawn Potter <potter.shawn@student.greenriver.edu>
2023-01-28 13:24:15 -0800

    changed styling

--- components/projects/ProjectItem.tsx
+++ components/projects/ProjectItem.tsx
@@ -9,19 +9,21 @@ interface Props
 	stack: string
 	projectUrl: string
 	buttonText: string
+	altText: string
+	ariaLabel: string
 }
 
-const ProjectItem: React.FC<Props> = ({title, backgroundImg, stack, projectUrl, buttonText}) =>
+const ProjectItem: React.FC<Props> = ({title, backgroundImg, stack, projectUrl, buttonText, altText, ariaLabel}) =>
 {
 
 	return(
-		<div className='relative flex items-center h-auto w-full shadow shadow-black rounded-xl p-4 group hover:bg-gradient-to-r from-blue-800 to-blue-600'>
-			<Image className='rounded-xl group-hover:opacity-10' src={backgroundImg} alt='' aria-label="Snek-Bot's logo of a simplistic snake" />
+		<div className='relative flex items-center h-auto w-full shadow shadow-black p-4 group hover:bg-gradient-to-r from-blue-800 to-blue-600'>
+			<Image className='m-auto group-hover:opacity-10' src={backgroundImg} alt={altText} aria-label={ariaLabel} />
 			<div className='hidden group-hover:block absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
 				<h3 className='text-2xl text-white tracking-widest text-center'>{title}</h3>
 				<p className='pb-4 pt-2 text-white text-center'>{stack}</p>
 				<Link href={projectUrl}>
-					<p className='text-center p-3 rounded-lg bg-white text-gray-900 font-bold text-lg cursor-pointer'>{buttonText}</p>
+					<p className='text-center p-3 bg-white text-gray-900 font-bold text-lg cursor-pointer'>{buttonText}</p>
 				</Link>
 			</div>
 		</div>
