ed93b699daaf8815737d2ff071d58d142b0f5678
Shawn Potter <potter.shawn@student.greenriver.edu>
2023-01-28 14:01:18 -0800

    added onClick event to mobile llinks

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -73,16 +73,16 @@ export default function Navbar()
 					<div className='py-4 flex flex-col'>
 						<ul className='uppercase'>
 							<Link href='/'>
-								<li className='py-4 text-sm'>Home</li>
+								<li onClick={() => setNav(false) } className='py-4 text-sm'>Home</li>
 							</Link>
 							<Link href='#about'>
-								<li className='py-4 text-sm'>About</li>
+								<li onClick={() => setNav(false) } className='py-4 text-sm'>About</li>
 							</Link>
 							<Link href='#skills'>
-								<li className='py-4 text-sm'>Skills</li>
+								<li onClick={() => setNav(false) } className='py-4 text-sm'>Skills</li>
 							</Link>
 							<Link href='#projects'>
-								<li className='py-4 text-sm'>Projects</li>
+								<li onClick={() => setNav(false) } className='py-4 text-sm'>Projects</li>
 							</Link>
 							{/* 
 							<Link href='contact'>
