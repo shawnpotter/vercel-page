987bea065a4062cf995b6769da726b02f6d4d7c3
Shawn Potter <potter.shawn@student.greenriver.edu>
2023-02-13 21:49:15 -0800

    removed unused import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Navbar from '../components/Navbar'
 import Greeting from '../components/Greeting'
 import About from '../components/About'
 import Skills from '../components/Skills'
 import Projects from '../components/Projects'
+import Contact from '../components/Contact'
 
 
 export default function Home() 
@@ -21,6 +21,7 @@ export default function Home()
       <About/>
       <Skills/>
       <Projects/>
+      <Contact/>
     </div>
   )
 }
