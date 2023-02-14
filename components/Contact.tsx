f2f8fb5dd410285ce5a0c2ae4dbbb634dc93e09d
Shawn Potter <potter.shawn@student.greenriver.edu>
2023-02-13 23:21:09 -0800

    added back to top button to if(state.succeeded)

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -9,8 +9,17 @@ export default function Contact() {
 	const [state, handleSubmit] = useForm(process.env.NEXT_PUBLIC_FORM as string)
 	if (state.succeeded) {
 		return (
-			<div className='flex justify-center py-20'>
-				<h2>Thank You For Messaging Me!</h2>
+			<div>
+				<div className='flex justify-center py-20'>
+					<h2>Thank You For Messaging Me!</h2>
+				</div>
+				<div className='flex justify-center py-12'>
+					<Link href='/'>
+						<div className='shadow shadow-black p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+							<HiOutlineChevronDoubleUp size={30} />
+						</div>
+					</Link>
+				</div>
 			</div>
 		)
 	}
