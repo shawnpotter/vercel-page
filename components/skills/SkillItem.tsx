c58577a7c6bbc0833770fc696ff6d013c079fe14
ShawnPotter <37090129+ShawnPotter@users.noreply.github.com>
2023-02-17 23:33:56 -0800

    reworked to accept svg paths

--- components/skills/SkillItem.tsx
+++ components/skills/SkillItem.tsx
@@ -1,19 +1,20 @@
+import Image from "next/image";
 import React from "react";
 
 interface Props 
 {
-	icon: React.ElementType
-	colorString: string
+	icon: string
 	name: string
+	altText: string
 }
 
-const SkillItem: React.FC<Props> =({icon, colorString, name}) =>
+const SkillItem: React.FC<Props> =({icon, name, altText}) =>
 {
 	return(
 		<div className='p-2 shadow shadow-black  hover:scale-105 ease-in duration-300'>
 			<div className='grid grid-cols-2 gap-4 justify-center items-center'>
-				<div className='m-auto'>
-					{React.createElement(icon, {size: 50, color: `${colorString}`})}
+				<div className='m-auto text-[3rem]'>
+					<Image src={icon} alt={altText} width={50} height={50}/>
 				</div>
 				<div>
 					<p className='text-xl'>{name}</p>
