bff85e66bd1cfc44cad7678a03df2424f4cfdb49
ShawnPotter <37090129+ShawnPotter@users.noreply.github.com>
2023-03-19 15:54:20 -0700

    updated grammar for About text

--- components/About.tsx
+++ components/About.tsx
@@ -11,7 +11,7 @@ export default function About() {
 				<div className='col-span-2'>
 					<h2 className='py-4'>About Me</h2>
 					<p className='py-2'>
-						In June of 2022 I graduated with my Bachelors in Software
+						In June of 2022, I graduated with my Bachelor's in Software
 						Development. It's been a long road getting to this point and hasn't
 						always been an easy one. Adversity, nor a pandemic, could stop me
 						from finally achieving my goals.
@@ -20,9 +20,10 @@ export default function About() {
 						Getting deeper into web development I realized that I had found a
 						niche, something that I can not just do, but be actively good at.
 						Since graduating I've spent my time expanding my tech stack and
-						working on projects. My focus has mostly on front-end and full-stack
-						development with an emphasis in React.js. However I've also taken
-						the time to learn other popular frameworks like AngularJS.
+						working on projects. My focus has mostly been on front-end and
+						full-stack development with an emphasis on React.js. However, I've
+						also taken the time to learn other popular frameworks like
+						AngularJS.
 					</p>
 					<div className='flex pt-20 justify-center md:mr-20 md:justify-end'>
 						<blockquote className='grid-row-2 text-lg italic p-3 bg-[#131518]/50 rounded-2xl w-full md:w-2/5 self-center font-roboto-slab'>
