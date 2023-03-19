7791024a3aa85684c1784886a87759a871be9369
ShawnPotter <37090129+ShawnPotter@users.noreply.github.com>
2023-03-19 13:33:15 -0700

    created new breakpoints and fixed positioning

--- components/Greeting.tsx
+++ components/Greeting.tsx
@@ -6,12 +6,12 @@ import { FaGithub, FaLinkedin } from 'react-icons/fa'
 
 export default function Greeting() {
 	return (
-		<div className='w-full h-screen text-center relative'>
+		<div className='w-full h-screen mdxl:h-3/4 lg:h-full text-center relative'>
 			<div className='flex h-full w-full z-0'>
-				<div className='absolute w-full h-[50rem] bg-black/[0.4] z-10'></div>
-				<div className='flex w-full h-[50rem] bg-planets bg-center bg-auto z-0'></div>
+				<div className='absolute w-full md:h-[50rem] lg:h-screen bg-black/[0.4] z-10'></div>
+				<div className='flex w-full md:h-[50rem] lg:h-screen bg-[#020101] bg-planets bg-center md:bg-auto z-0'></div>
 			</div>
-			<div className='flex h-full w-full p-2 -top-12 justify-center items-center z-20 absolute'>
+			<div className='flex h-full md:h-4/5 mdxl:h-full lg:h-screen w-full p-2 -top-2 md:-top-0 justify-center items-center z-20 absolute'>
 				<div>
 					<Image
 						className='py-2 mx-auto'
@@ -29,7 +29,7 @@ export default function Greeting() {
 							href='https://www.linkedin.com/in/shawn-potter/'
 							aria-label="Link to Shawn's LinkedIn page"
 						>
-							<div className='bg-[#0077b6] shadow-md shadow-gray-900 rounded-xl p-5 cursor-pointer hover:scale-105 hover:bg-[#0d4b6d] ease-in duration-300'>
+							<div className='bg-[#0077b6] shadow-md shadow-gray-900 rounded-xl p-4 sm:p-5 cursor-pointer hover:scale-105 hover:bg-[#0d4b6d] ease-in duration-300'>
 								<FaLinkedin
 									className='text-2xl'
 									color='#e6e6e6'
@@ -40,7 +40,7 @@ export default function Greeting() {
 							href='https://github.com/shawnpotter'
 							aria-label="Link to Shawn's GitHub page"
 						>
-							<div className='bg-[#0077b6] shadow-md shadow-gray-900 rounded-xl p-5 cursor-pointer hover:scale-105 hover:bg-[#0d4b6d] ease-in duration-300'>
+							<div className='bg-[#0077b6] shadow-md shadow-gray-900 rounded-xl p-4 sm:p-5 cursor-pointer hover:scale-105 hover:bg-[#0d4b6d] ease-in duration-300'>
 								<FaGithub
 									className='text-2xl'
 									color='#e6e6e6'
@@ -51,7 +51,7 @@ export default function Greeting() {
 							href='#contact'
 							aria-label='Click to send an email to Shawn'
 						>
-							<div className='bg-[#0077b6] shadow-md shadow-gray-900 rounded-xl p-5 cursor-pointer hover:scale-105 hover:bg-[#0d4b6d] ease-in duration-300'>
+							<div className='bg-[#0077b6] shadow-md shadow-gray-900 rounded-xl p-4 sm:p-5 cursor-pointer hover:scale-105 hover:bg-[#0d4b6d] ease-in duration-300'>
 								<AiOutlineMail
 									className='text-2xl'
 									color='#e6e6e6'
