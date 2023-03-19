7b35c0dc567e66142349f31dad8ab5f49c62dd60
ShawnPotter <37090129+ShawnPotter@users.noreply.github.com>
2023-03-18 23:30:41 -0700

    updated stylings, color, and fonts

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { AiOutlineMail } from 'react-icons/ai'
 import { FaGithub, FaLinkedin } from 'react-icons/fa'
-import { useForm, ValidationError } from '@formspree/react';
+import { useForm, ValidationError } from '@formspree/react'
 
 export default function Contact() {
 	const [state, handleSubmit] = useForm(process.env.NEXT_PUBLIC_FORM as string)
@@ -14,105 +14,194 @@ export default function Contact() {
 			</div>
 		)
 	}
-  return (
-
-	<div id='contact' className='w-full lg:h-5/6 p-5'>
-		<div className='mx-auto max-w-7xl py-16 flex flex-col justify-center h-full'>
-			<h2 className='py-4'>Contact Me</h2>
-			<div className='grid lg:grid-cols-5 gap-8 text-black'>
-				{/* Left */}
-				<div className='bg-[#F4F3EE] col-span-3 lg:col-span-2 w-full h-full rounded shadow-md shadow-gray-900 px-4'>
-					<div className='lg:p-4 h-full'>
-						{/* picture */}
-						{/* 
+	return (
+		<div
+			id='contact'
+			className='w-full lg:h-5/6 p-5'
+		>
+			<div className='mx-auto max-w-7xl py-16 flex flex-col justify-center h-full'>
+				<h2 className='py-4'>Contact Me</h2>
+				<div className='grid lg:grid-cols-5 gap-8 text-gray-50'>
+					{/* Left */}
+					<div className='bg-[#495057] col-span-3 lg:col-span-2 w-full h-full rounded-xl shadow-md shadow-gray-900 px-4'>
+						<div className='lg:p-4 h-full'>
+							{/* picture */}
+							{/* 
 						<div>
 							<img className='hover:scale-105 ease-in duration-300' src='' />
 						</div> 
 						*/}
-						<div>
-							<h2 className='py-2'>Shawn Potter</h2>
-							<p>Software Engineer</p>
-							<p className='py-4'>I am available for full-time positions. Contact me and let's talk.</p>
-						</div>
-						<div>
-							<p className='uppercase pt-8 font-semibold'>Connect With Me</p>
-							<div className='flex items-center justify-between max-w-xs m-auto py-4'>
-								<div className='bg-[#BCB8B1] rounded-xl shadow-md shadow-gray-900 p-5 cursor-pointer hover:scale-105 hover:bg-[#B2B2BD] ease-in duration-200'>
-									<a href='https://www.linkedin.com/in/shawn-potter/' aria-label="Link to Shawn's LinkedIn page">
-										<FaLinkedin className='text-2xl' />
-									</a>
-								</div>
-								<div className='bg-[#BCB8B1] rounded-xl shadow-md shadow-gray-900 p-5 cursor-pointer hover:scale-105 hover:bg-[#B2B2BD] ease-in duration-200'>
-									<a href='https://github.com/shawnpotter' aria-label="Link to Shawn's GitHub page">
-										<FaGithub className='text-2xl' />
-									</a>
-								</div>
-								<div className='bg-[#BCB8B1] rounded-xl shadow-md shadow-gray-900 p-5 cursor-pointer hover:scale-105 hover:bg-[#B2B2BD] ease-in duration-200'>
-									<a href='mailto:shawnpotter.inquires@gmail.com' aria-label="Click to send an email to Shawn">
-										<AiOutlineMail className='text-2xl' />
-									</a>
+							<div>
+								<h2 className='py-2'>Shawn Potter</h2>
+								<p>Software Engineer</p>
+								<p className='py-4'>
+									I am available for full-time positions. Contact me and let's
+									talk.
+								</p>
+							</div>
+							<div>
+								<p className='uppercase pt-8 font-semibold'>Connect With Me</p>
+								<div className='flex items-center justify-between max-w-xs m-auto py-4 text-black'>
+									<div className='bg-[#0077b6] rounded-xl shadow-md shadow-gray-900 p-5 cursor-pointer hover:scale-105 hover:bg-[#0d4b6d] ease-in duration-200'>
+										<a
+											href='https://www.linkedin.com/in/shawn-potter/'
+											aria-label="Link to Shawn's LinkedIn page"
+										>
+											<FaLinkedin
+												className='text-2xl'
+												color='#e6e6e6'
+											/>
+										</a>
+									</div>
+									<div className='bg-[#0077b6] rounded-xl shadow-md shadow-gray-900 p-5 cursor-pointer hover:scale-105 hover:bg-[#0d4b6d] ease-in duration-200'>
+										<a
+											href='https://github.com/shawnpotter'
+											aria-label="Link to Shawn's GitHub page"
+										>
+											<FaGithub
+												className='text-2xl'
+												color='#e6e6e6'
+											/>
+										</a>
+									</div>
+									<div className='bg-[#0077b6] rounded-xl shadow-md shadow-gray-900 p-5 cursor-pointer hover:scale-105 hover:bg-[#0d4b6d] ease-in duration-200'>
+										<a
+											href='mailto:shawnpotter.inquires@gmail.com'
+											aria-label='Click to send an email to Shawn'
+										>
+											<AiOutlineMail
+												className='text-2xl'
+												color='#e6e6e6'
+											/>
+										</a>
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-				{/* Right */}
-				<div className='bg-[#F4F3EE] col-span-3 w-full h-auto rounded shadow-md shadow-gray-900 lg:p-4'>
-					<div className='p-4'>
-						<form onSubmit={handleSubmit}>
-							<div className='grid md:grid-cols-2 gap-4 w-full py-2'>
-								<div className='flex flex-col'>
-									<label className='uppercase text-sm py-2' htmlFor='name'>Name</label>
-									<input className='rounded border-2 p-3 flex bg-[#F4F3EE] border-[#463F3A] placeholder:text-[#463F3A] placeholder:opacity-80' type="text" id='name' name='name' placeholder='First and Last' required />
-									<ValidationError 
-										prefix="name" 
-										field="name"
+					{/* Right */}
+					<div className='bg-[#495057] text-white col-span-3 w-full h-auto rounded-xl shadow-md shadow-gray-900 lg:p-4'>
+						<div className='p-4'>
+							<form onSubmit={handleSubmit}>
+								<div className='grid md:grid-cols-2 gap-4 w-full py-2'>
+									<div className='flex flex-col'>
+										<label
+											className='uppercase text-sm py-2 font-semibold'
+											htmlFor='name'
+										>
+											Name
+										</label>
+										<input
+											className='rounded-xl border-2 p-3 flex bg-[#212529] border-[#6c757d] placeholder:text-gray-50 placeholder:opacity-80'
+											type='text'
+											id='name'
+											name='name'
+											placeholder='First and Last'
+											required
+										/>
+										<ValidationError
+											prefix='name'
+											field='name'
+											errors={state.errors}
+										/>
+									</div>
+									<div className='flex flex-col'>
+										<label
+											className='uppercase text-sm py-2 font-semibold'
+											htmlFor='number'
+										>
+											Number
+										</label>
+										<input
+											className='rounded-xl border-2 p-3 flex bg-[#212529] border-[#6c757d] placeholder:text-gray-50 placeholder:opacity-80'
+											type='telephone'
+											id='number'
+											name='number'
+											placeholder='(555) 555-5555'
+											required
+										/>
+										<ValidationError
+											prefix='number'
+											field='number'
+											errors={state.errors}
+										/>
+									</div>
+								</div>
+								<div className='flex flex-col py-2'>
+									<label
+										className='uppercase text-sm py-2 font-semibold'
+										htmlFor='email'
+									>
+										Email
+									</label>
+									<input
+										className='rounded-xl border-2 p-3 flex bg-[#212529] border-[#6c757d] placeholder:text-gray-50 placeholder:opacity-80'
+										type='email'
+										id='email'
+										name='_replyto'
+										placeholder='email@example.com'
+										required
+									/>
+									<ValidationError
+										prefix='email'
+										field='email'
 										errors={state.errors}
 									/>
 								</div>
-								<div className='flex flex-col'>
-									<label className='uppercase text-sm py-2' htmlFor='number'>Number</label>
-									<input className='rounded border-2 p-3 flex bg-[#F4F3EE] border-[#463F3A] placeholder:text-[#463F3A] placeholder:opacity-80' type='telephone' id='number' name='number' placeholder='(555) 555-5555' required />
-									<ValidationError 
-										prefix="number" 
-										field="number"
+								<div className='flex flex-col py-2'>
+									<label
+										className='uppercase text-sm py-2 font-semibold'
+										htmlFor='subject'
+									>
+										Subject
+									</label>
+									<input
+										className='rounded-xl border-2 p-3 flex bg-[#212529] border-[#6c757d] placeholder:text-gray-50 placeholder:opacity-80'
+										type='text'
+										id='subject'
+										name='_subject'
+										placeholder='Subject'
+										required
+									/>
+									<ValidationError
+										prefix='subject'
+										field='subject'
 										errors={state.errors}
 									/>
 								</div>
-							</div>
-							<div className='flex flex-col py-2'>
-								<label className='uppercase text-sm py-2' htmlFor='email'>Email</label>
-								<input className='rounded border-2 p-3 flex bg-[#F4F3EE] border-[#463F3A] placeholder:text-[#463F3A] placeholder:opacity-80' type="email" id='email' name='_replyto' placeholder='email@example.com' required />
-								<ValidationError 
-									prefix="email" 
-									field="email"
-									errors={state.errors}
-								/>
-							</div>
-							<div className='flex flex-col py-2'>
-								<label className='uppercase text-sm py-2' htmlFor='subject'>Subject</label>
-								<input className='rounded border-2 p-3 flex bg-[#F4F3EE] border-[#463F3A] placeholder:text-[#463F3A] placeholder:opacity-80' type="text" id='subject' name='_subject' placeholder='Subject' required />
-								<ValidationError 
-									prefix="subject" 
-									field="subject"
-									errors={state.errors}
-								/>
-							</div>
-							<div className='flex flex-col py-2'>
-								<label className='uppercase text-sm py-2' htmlFor='message'>Message</label>
-								<textarea className='rounded border-2 p-3 flex bg-[#F4F3EE] border-[#463F3A] placeholder:text-[#463F3A] placeholder:opacity-80' rows={10} id='message' name='message' placeholder='Ask me anything...' required/>
-								<ValidationError 
-									prefix="message" 
-									field="message"
-									errors={state.errors}
-								/>
-							</div>
-							<button className='w-full p-4 mt-4 text-black font-semibold text disabled:shadow rounded shadow-black uppercase bg-[#E0AFA0]' type='submit' disabled={state.submitting}>Send Message</button>
-						</form>
+								<div className='flex flex-col py-2'>
+									<label
+										className='uppercase text-sm py-2 font-semibold'
+										htmlFor='message'
+									>
+										Message
+									</label>
+									<textarea
+										className='rounded-xl border-2 p-3 flex bg-[#212529] border-[#6c757d] placeholder:text-gray-50 placeholder:opacity-80'
+										rows={10}
+										id='message'
+										name='message'
+										placeholder='Ask me anything...'
+										required
+									/>
+									<ValidationError
+										prefix='message'
+										field='message'
+										errors={state.errors}
+									/>
+								</div>
+								<button
+									className='w-full p-4 mt-4 text-gray-50 font-semibold text disabled:shadow rounded-xl shadow-black uppercase bg-[#0077b6]'
+									type='submit'
+									disabled={state.submitting}
+								>
+									Send Message
+								</button>
+							</form>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-  )
+	)
 }
--- components/Greeting.tsx
+++ components/Greeting.tsx
@@ -1,47 +1,65 @@
-import React from 'react';
-import Image from 'next/image';
+import React from 'react'
+import Image from 'next/image'
 import logoLarge from '../public/assets/shawnpotter.png'
-import { AiOutlineMail } from 'react-icons/ai';
+import { AiOutlineMail } from 'react-icons/ai'
 /* import { BsFillPersonLinesFill } from 'react-icons/bs'; */
-import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
 
-export default function Greeting() 
-{
+export default function Greeting() {
 	return (
-		<div className='w-full h-screen text-center'>
-			<div className='max-w-7xl w-full h-full mx-auto p-2 flex justify-center items-center'>
+		<div className='w-full h-screen text-center relative'>
+			<div className='flex h-full w-full z-0'>
+				<div className='absolute w-full h-[50rem] bg-black/[0.4] z-10'></div>
+				<div className='flex w-full h-[50rem] bg-planets bg-center bg-auto z-0'></div>
+			</div>
+			<div className='flex h-full w-full p-2 -top-12 justify-center items-center z-20 absolute'>
 				<div>
-
-					{/* Tagline location */}
-{/* 
-					<p className='uppercase text-sm tracking-widest text-gray-50'>
-						
-					</p>
- */}
-					<Image className='py-2 mx-auto' src={logoLarge} alt=''/>
-					<h1 className='py-2 text-gray-50'>
-						Software Engineer
-					</h1>
-					<p className='py-4 text-gray-100 max-w-[70%] m-auto'>
-						I specialize in front-end and full-stack development.
+					<Image
+						className='py-2 mx-auto'
+						src={logoLarge}
+						alt=''
+					/>
+					<h1 className='py-2 text-gray-100'>Software Engineer</h1>
+					<p className='py-4 text-gray-300 max-w-[70%] font-semibold m-auto'>
+						Full Stack Developer
+						<br />
+						Based in the Pacific Northwest
 					</p>
 					<div className='flex items-center justify-between max-w-xs m-auto py-4'>
-						<div className='bg-[#BCB8B1] shadow-md shadow-gray-900 rounded-xl p-5 cursor-pointer hover:scale-105 hover:bg-[#B2B2BD] ease-in duration-300'>
-							<a href='https://www.linkedin.com/in/shawn-potter/' aria-label="Link to Shawn's LinkedIn page">
-								<FaLinkedin className='text-2xl' color='black' />
-							</a>
-						</div>
-						<div className='bg-[#BCB8B1] shadow-md shadow-gray-900 rounded-xl p-5 cursor-pointer hover:scale-105 hover:bg-[#B2B2BD] ease-in duration-300'>
-							<a href='https://github.com/shawnpotter' aria-label="Link to Shawn's GitHub page">
-								<FaGithub className='text-2xl' color='black' />
-							</a>
-						</div>
-						<div className='bg-[#BCB8B1] shadow-md shadow-gray-900 rounded-xl p-5 cursor-pointer hover:scale-105 hover:bg-[#B2B2BD] ease-in duration-300'>
-							<a href='mailto:shawnpotter.inquires@gmail.com' aria-label="Click to send an email to Shawn">
-								<AiOutlineMail className='text-2xl' color='black' />
-							</a>
-						</div>
-{/* 						
+						<a
+							href='https://www.linkedin.com/in/shawn-potter/'
+							aria-label="Link to Shawn's LinkedIn page"
+						>
+							<div className='bg-[#0077b6] shadow-md shadow-gray-900 rounded-xl p-5 cursor-pointer hover:scale-105 hover:bg-[#0d4b6d] ease-in duration-300'>
+								<FaLinkedin
+									className='text-2xl'
+									color='#e6e6e6'
+								/>
+							</div>
+						</a>
+						<a
+							href='https://github.com/shawnpotter'
+							aria-label="Link to Shawn's GitHub page"
+						>
+							<div className='bg-[#0077b6] shadow-md shadow-gray-900 rounded-xl p-5 cursor-pointer hover:scale-105 hover:bg-[#0d4b6d] ease-in duration-300'>
+								<FaGithub
+									className='text-2xl'
+									color='#e6e6e6'
+								/>
+							</div>
+						</a>
+						<a
+							href='mailto:shawnpotter.inquires@gmail.com'
+							aria-label='Click to send an email to Shawn'
+						>
+							<div className='bg-[#0077b6] shadow-md shadow-gray-900 rounded-xl p-5 cursor-pointer hover:scale-105 hover:bg-[#0d4b6d] ease-in duration-300'>
+								<AiOutlineMail
+									className='text-2xl'
+									color='#e6e6e6'
+								/>
+							</div>
+						</a>
+						{/* 						
 						<div className='shadow shadow-black p-5 cursor-pointer hover:scale-105 ease-in duration-300'>
 							<a href='/' aria-label="">
 								<BsFillPersonLinesFill className='text-2xl' />
