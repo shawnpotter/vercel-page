827cc7f8345298a53ac0c8bd61f7b4851b8dd082
ShawnPotter <37090129+ShawnPotter@users.noreply.github.com>
2023-03-18 23:26:41 -0700

    added fontFamily and backgroundImage

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,24 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
-  ],
-  theme: {
-    screens: {
-      'xs': '475px',
-      ...defaultTheme.screens,
-    },
-  },
-  plugins: [],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Lato', 'sans'],
+				'roboto-slab': ['Roboto Slab', 'serif'],
+			},
+			backgroundImage: {
+				planets: "url('../public/assets/MobileWallpaper.png')",
+			},
+		},
+		screens: {
+			xs: '475px',
+			...defaultTheme.screens,
+		},
+	},
+	plugins: [],
 }
